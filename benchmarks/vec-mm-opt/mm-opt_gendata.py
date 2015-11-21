23171fa0ec1c7ea35c122be7c50e0d23a0957f92
Sagar Karandikar <karandikarsagar@gmail.com>
2015-11-21 00:11:54 -0800

    11 register version of mm-opt, 5.376050268 fl ops / cycle

--- benchmarks/vec-mm-opt/mm-opt_gendata.py
+++ benchmarks/vec-mm-opt/mm-opt_gendata.py
@@ -1,6 +1,6 @@
 import random
 
-data_size = 24
+data_size = 64
 
 X = [int(random.random()*1000.00) for x in range(data_size*data_size)]
 Y = [int(random.random()*1000.00) for x in range(data_size*data_size)]
