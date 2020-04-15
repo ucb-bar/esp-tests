cca0585509d3197f65010f608ecacf4ef9de0aa8
Colin Schmidt <colins@eecs.berkeley.edu>
2020-04-14 19:01:04 -0700

    Add vtmm dgemm and more isa tests

---
+++ benchmarks/vec-dgemm-opt-vtmm/vec-dgemm-opt_main.c
@@ -0,0 +1,28 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Vector Matrix Multiply Naive benchmark for Hwacha v4
+//--------------------------------------------------------------------------
+//
+
+#include "util.h"
+#include "vec-dgemm-opt.h"
+
+//--------------------------------------------------------------------------
+// Input/Reference Data
+
+#include "dataset64.h"
+
+//--------------------------------------------------------------------------
+// Main
+
+int main( int argc, char* argv[] )
+{
+  // Do the saxpy
+  setStats(1);
+  vec_dgemm_opt_c(MAT_WIDTH, result_holder, input_data_X, input_data_Y);
+  setStats(0);
+
+  // Check the results
+  return verifyDouble(MAT_WIDTH*MAT_WIDTH, result_holder, verify_data);
+}
